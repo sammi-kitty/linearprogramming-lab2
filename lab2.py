98deeeb0312506f921f20393c0fc6d75c00a7f3a
sammi-kitty <samantha.agren@icloud.com>
2026-05-02 18:50:38 +0200

    trying to fix plots, toying with history

--- lab2.py
+++ lab2.py
@@ -6,6 +6,10 @@ ITERATIONS = 1000
 
 def main():
     original_data = np.loadtxt(r"SP500.txt")
+    name_data = []
+    with open("stock_names.txt", "r") as file:
+        for line in file:
+            name_data.append(str(line))
 
     # Get average of the historical returns
     historical_avg = np.average(original_data, axis=0)
@@ -23,27 +27,51 @@ def main():
     history_agnostic = cgm_implementation(init_guess, historical_avg, variance, inflation, 1)
     history_greedy = cgm_implementation(init_guess, historical_avg, variance, inflation, 2)
 
-    # pyplot.figure()
-
-    # pyplot.subplot(121)
+    
+    # Plot current risk
     pyplot.plot(history_agnostic[0], label="Agnostic")
     pyplot.plot(history_greedy[0], label="Greedy")
     pyplot.xlabel('Iteration')
     pyplot.ylabel('Risk')
+    pyplot.title("Risk för $x^{(k)}$ given av CGM, för steglängd $greedy$ och $agnostic$.")
     pyplot.legend()
-    pyplot.show()
+    pyplot.savefig(f"{ITERATIONS}-iter-risk.png", dpi=400)
+    pyplot.clf()
 
-    '''pyplot.subplot(122)
-    # pyplot.plot(history_agnostic[1], label = "Stock portion")
-    x_composition_history = history_agnostic[1]
-    for i in range(25):
-        stock = x_composition_history[:, i]
-        pyplot.plot(stock, label=f"Stock x_{i}")
-        
+    # Plot current profit
+    pyplot.plot(history_agnostic[2], label="Agnostic")
+    pyplot.plot(history_greedy[2], label="Greedy")
     pyplot.xlabel('Iteration')
-    pyplot.ylabel('')
+    pyplot.ylabel('Avkastning')
+    pyplot.title("Avkastning för $x^{(k)}$ given av CGM, för steglängd $greedy$ och $agnostic$.")
     pyplot.legend()
-    pyplot.show()'''
+    pyplot.savefig(f"{ITERATIONS}-iter-profit.png", dpi=400)
+    pyplot.clf()
+
+    fig, ax = pyplot.subplots(figsize = (7, 2.5))
+    x_composition_history = history_greedy[1]
+    i = 0
+    for stock in x_composition_history:
+        ax.plot(stock, label=f'{name_data[i]}')
+        i = i + 1
+    ax.set_ylabel('Del av portfölj, andel av 1')
+    ax.set_xlabel('Iteration')
+    fig.legend(fontsize = 'xx-small', loc='outside right upper', labelspacing=0.2,)
+    fig.savefig(f"{ITERATIONS}-iter-composition.png", dpi=400)
+    
+
+    agnostic_solution = history_agnostic[3]
+    greedy_solution = history_greedy[3]
+
+    # print(f"Agnostic solution: {history_agnostic[3]}")
+    # print(f"Greedy solution: {history_greedy[3]}")
+    for i in range(25):
+        print(
+            f'''{name_data[i]} &
+            {100 * agnostic_solution[i]:.6f}\% &
+            {100 * greedy_solution[i]:.6f}\%
+            \\\\ \hline''')
+    
 
 def solve_lp(historical_avg, inflation, c):
 
@@ -65,18 +93,23 @@ def solve_lp(historical_avg, inflation, c):
 
 
 def cgm_implementation(init_guess, historical_avg, variance, inflation, stepsize_type):
-    return_history = []
-    x_composition_history = np.zeros([ITERATIONS, 25])
+    risk_history = []
+    x_composition_history = np.empty([ITERATIONS, 25])
+    profit_history = []
     x = init_guess
+    has_converged = False
     for i in range(1, ITERATIONS + 1):
         c = 2 * variance * x # LP coefficient
 
         y = solve_lp(historical_avg, inflation, c)
 
-        return_history.append(
+        risk_history.append(
             np.sum(np.square(x) * variance)
         )
-        np.insert(x_composition_history, i, x, axis=0)
+        profit_history.append(
+            np.sum(x * historical_avg)
+        )
+        x_composition_history[i - 1] = x
 
 
         if stepsize_type == 1:
@@ -86,14 +119,25 @@ def cgm_implementation(init_guess, historical_avg, variance, inflation, stepsize
         else:
             return
 
+        
+        if np.linalg.norm(x_composition_history[i-1] - x_composition_history[i-2]) == 0 and not(has_converged) and i > 5:
+            print(f"Converged at iteration {i} for type {stepsize_type}")
+            has_converged = True
+            
+            
+
 
         x = ((1 - h) * x) + (h * y)
- 
-    # print(x_composition_history)
-    return [return_history, x_composition_history, x]
+
+    composition_history_star = [None] * 25
+    for i in range(25):
+        composition_history_star[i] = x_composition_history[:, i]
+    
+    return [risk_history, composition_history_star, profit_history, x]
 
 def initial_guess(historical_avg, inflation):
-    c = np.ones(25)
+    '''
+    c = np.ones(25) + 1
     
     # Convert historical_avg @ x >= inflation to 
     # -historical_avg @ x <= -inflation
@@ -110,6 +154,9 @@ def initial_guess(historical_avg, inflation):
         c = c, A_ub = A_ub, b_ub = b_ub, A_eq = A_eq,
         b_eq = b_eq, bounds = bounds, method = 'highs'
         ).x
+    '''
+    naive_start = np.ones(25) / 25
+    return naive_start
     
 
 def greedy_step_size(x, y, variance):
