f3ac52a51ddf71dc297d10a78df1d7de4bdb20fe
sammi-kitty <samantha.agren@icloud.com>
2026-05-04 22:02:07 +0200

    sync

--- lab2.py
+++ lab2.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.optimize import linprog
 import matplotlib.pyplot as pyplot
 
-ITERATIONS = 10000
+ITERATIONS = 1000
 
 N = 25
 
@@ -20,27 +20,46 @@ def main():
     variance = np.average(np.square(original_data - historical_avg), axis=0)
 
     # Get inflation (basic implementation)
-    inflation = np.average(historical_avg)
+    alpha1 = np.average(historical_avg)
+    alpha2 = (1/2) * (np.average(historical_avg) + np.max(historical_avg))
 
     # Set init guess
-    init_guess = initial_guess(historical_avg, inflation)
+    init_guess = initial_guess(historical_avg, alpha2)
+    print(np.sum(historical_avg * init_guess) - alpha2)
 
     # Run CGM
-    history_agnostic = cgm_implementation(init_guess, historical_avg, variance, inflation, 1)
-    history_greedy = cgm_implementation(init_guess, historical_avg, variance, inflation, 2)
+    alpha1_history_agnostic = cgm_implementation(init_guess, historical_avg, variance, alpha1, 1)
+    alpha1_history_greedy = cgm_implementation(init_guess, historical_avg, variance, alpha1, 2)
+
+    alpha2_history_agnostic = cgm_implementation(init_guess, historical_avg, variance, alpha2, 1)
+    alpha2_history_greedy = cgm_implementation(init_guess, historical_avg, variance, alpha2, 2)
 
     # Plot graphs
-    plotting(history_greedy, history_agnostic, name_data)
+    plotting(alpha1_history_greedy, alpha1_history_agnostic, alpha2_history_greedy, alpha2_history_agnostic, name_data)
 
     # Print LaTeX-ready tabular of solutions
-    agnostic_solution = history_agnostic[3]
-    greedy_solution = history_greedy[3]
+    alpha1_agnostic_solution = alpha1_history_agnostic[3]
+    alpha1_greedy_solution = alpha1_history_greedy[3]
+    alpha2_agnostic_solution = alpha2_history_agnostic[3]
+    alpha2_greedy_solution = alpha2_history_greedy[3]
     # for i in range(N):
     #     print(
     #         f'''{name_data[i]} &
-    #         {100 * agnostic_solution[i]:.6f}\% &
-    #         {100 * greedy_solution[i]:.6f}\%
+    #         {100 * alpha1_agnostic_solution[i]:.6f}\% &
+    #         {100 * alpha1_greedy_solution[i]:.6f}\% &
+    #         {100 * alpha2_agnostic_solution[i]:.6f}\% &
+    #         {100 * alpha2_greedy_solution[i]:.6f}\%
     #         \\\\ \hline''')
+
+    print(f"Avkastning för alpha1 agnostic: {np.sum(historical_avg * alpha1_agnostic_solution):.10f}")
+    print(f"Avkastning för alpha1 greedy: {np.sum(historical_avg * alpha1_greedy_solution):.10f}")
+    print(f"Avkastning för alpha2 agnostic: {np.sum(historical_avg * alpha2_agnostic_solution):.10f}")
+    print(f"Avkastning för alpha2 greedy: {np.sum(historical_avg * alpha2_greedy_solution):.10f}")
+
+    print(f"Risk för alpha1 agnostic: {np.sum(variance * np.square(alpha1_agnostic_solution)):.10f}")
+    print(f"Risk för alpha1 greedy: {np.sum(variance * np.square(alpha1_greedy_solution)):.10f}")
+    print(f"Risk för alpha2 agnostic: {np.sum(variance * np.square(alpha2_agnostic_solution)):.10f}")
+    print(f"Risk för alpha2 greedy: {np.sum(variance * np.square(alpha2_greedy_solution)):.10f}")
 
 def solve_lp(historical_avg, inflation, c):
 
@@ -80,13 +99,12 @@ def cgm_implementation(init_guess, historical_avg, variance, inflation, stepsize
         )
         x_composition_history[i - 1] = x
 
-
         if stepsize_type == 1:
             h = agnostic_step_size(i)
         elif stepsize_type == 2:
             h = greedy_step_size(x, y, variance)
-            if not(has_converged):
-                print(f"{i} & {h} \\\\ \hline")
+            # if not(has_converged):
+               # print(f"{i} & {h} \\\\ \hline")
         else:
             return
 
@@ -106,10 +124,24 @@ def cgm_implementation(init_guess, historical_avg, variance, inflation, stepsize
     return [risk_history, composition_history_star, profit_history, x]
 
 def initial_guess(historical_avg, inflation):
-    naive_start = np.ones(N) / N
-    naive_start[17] = 3/25
-    naive_start[20] = 0
-    naive_start[13] = 0
+    c = np.ones(N) / N
+
+    # Convert historical_avg @ x >= inflation to 
+    # -historical_avg @ x <= -inflation
+    A_ub = [-historical_avg]
+    b_ub = [-inflation]
+
+    # Make sure that sum(x) == 1
+    A_eq = [np.ones(N)]
+    b_eq = [1]
+
+    bounds = [(0, None)] * N
+
+    naive_start = linprog(
+        c = c, A_ub = A_ub, b_ub = b_ub, A_eq = A_eq,
+        b_eq = b_eq, bounds = bounds, method = 'highs'
+        ).x
+
     return naive_start
 
 def greedy_step_size(x, y, variance):
@@ -140,32 +172,36 @@ def greedy_step_size(x, y, variance):
 def agnostic_step_size(i):
     return 2 / (i + 1)
 
-def plotting(history_greedy, history_agnostic, name_data):
+def plotting(alpha1_history_greedy, alpha1_history_agnostic, alpha2_history_greedy, alpha2_history_agnostic, name_data):
     # Plot risk
-    pyplot.plot(history_agnostic[0], label="Agnostic")
-    pyplot.plot(history_greedy[0], label="Greedy")
+    pyplot.plot(alpha1_history_agnostic[0], label="Agnostic $\\alpha_1$")
+    pyplot.plot(alpha1_history_greedy[0], label="Greedy $\\alpha_1$")
+    pyplot.plot(alpha2_history_agnostic[0], label="Agnostic $\\alpha_2$")
+    pyplot.plot(alpha2_history_greedy[0], label="Greedy $\\alpha_2$")
     pyplot.xlabel('Iteration $k$')
     pyplot.ylabel('Risk')
     pyplot.title("Risk för $x^{(k)}$ given av CGM, för steglängd $greedy$ och $agnostic$.")
     pyplot.legend()
-    pyplot.savefig(f"{ITERATIONS}-iter-risk.png", dpi=400)
+    pyplot.savefig(f"risk/{ITERATIONS}-iter-risk.png", dpi=400)
     pyplot.clf()
 
     # Plot profit
-    pyplot.plot(history_agnostic[2], label="Agnostic")
-    pyplot.plot(history_greedy[2], label="Greedy")
+    pyplot.plot(alpha1_history_agnostic[2], label="Agnostic $\\alpha_1$")
+    pyplot.plot(alpha1_history_greedy[2], label="Greedy $\\alpha_1$")
+    pyplot.plot(alpha2_history_agnostic[2], label="Agnostic $\\alpha_2$")
+    pyplot.plot(alpha2_history_greedy[2], label="Greedy $\\alpha_2$")
     pyplot.xlabel('Iteration $k$')
     pyplot.ylabel('Avkastning')
     pyplot.title("Avkastning för $x^{(k)}$ given av CGM, för steglängd $greedy$ och $agnostic$.")
     pyplot.legend()
-    pyplot.savefig(f"{ITERATIONS}-iter-profit.png", dpi=400)
+    pyplot.savefig(f"profit/{ITERATIONS}-iter-profit.png", dpi=400)
     pyplot.clf()
 
     # Plot composition (one for agnostic and one for greedy)
     # Use different linestyles since colours run out
     fig = pyplot.figure()
     ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
-    x_composition_history = history_greedy[1]
+    x_composition_history = alpha1_history_greedy[1]
     i = 0
     for stock in x_composition_history:
         if i < 10:
@@ -177,13 +213,13 @@ def plotting(history_greedy, history_agnostic, name_data):
         i = i + 1
     ax.set_ylabel('Del av portfölj, andel av 1')
     ax.set_xlabel('Iteration $k$')
-    ax.set_title('Portföljkomposition $x^{(k)}$, greedy')
+    ax.set_title('Portföljkomposition $x^{(k)}$, greedy, $\\alpha_1$')
     fig.legend(fontsize = 'xx-small', loc='outside right upper', labelspacing=0.2,)
-    fig.savefig(f"{ITERATIONS}-iter-composition-greedy.png", dpi=400)
+    fig.savefig(f"composition/{ITERATIONS}-iter-composition-greedy-alpha1.png", dpi=400)
 
     fig = pyplot.figure()
     ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
-    x_composition_history = history_agnostic[1]
+    x_composition_history = alpha1_history_agnostic[1]
     i = 0
     for stock in x_composition_history:
         if i < 10:
@@ -195,8 +231,44 @@ def plotting(history_greedy, history_agnostic, name_data):
         i = i + 1
     ax.set_ylabel('Del av portfölj, andel av 1')
     ax.set_xlabel('Iteration $k$')
-    ax.set_title('Portföljkomposition $x^{(k)}$, agnostic')
+    ax.set_title('Portföljkomposition $x^{(k)}$, agnostic, $\\alpha_1$')
     fig.legend(fontsize = 'xx-small', loc='outside right upper', labelspacing=0.2,)
-    fig.savefig(f"{ITERATIONS}-iter-composition-agnostic.png", dpi=400)
+    fig.savefig(f"composition/{ITERATIONS}-iter-composition-agnostic-alpha1.png", dpi=400)
+
+    fig = pyplot.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
+    x_composition_history = alpha2_history_greedy[1]
+    i = 0
+    for stock in x_composition_history:
+        if i < 10:
+            ax.plot(stock, label=f'$x_{{ {i + 1} }}$, {name_data[i]}')
+        elif 10 <= i < 20:
+            ax.plot(stock, label=f'$x_{{ {i + 1} }}$, {name_data[i]}', linestyle='--')
+        else:
+            ax.plot(stock, label=f'$x_{{ {i + 1} }}$, {name_data[i]}', linestyle=':')
+        i = i + 1
+    ax.set_ylabel('Del av portfölj, andel av 1')
+    ax.set_xlabel('Iteration $k$')
+    ax.set_title('Portföljkomposition $x^{(k)}$, greedy, $\\alpha_2$')
+    fig.legend(fontsize = 'xx-small', loc='outside right upper', labelspacing=0.2,)
+    fig.savefig(f"composition/{ITERATIONS}-iter-composition-greedy-alpha2.png", dpi=400)
+
+    fig = pyplot.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
+    x_composition_history = alpha2_history_agnostic[1]
+    i = 0
+    for stock in x_composition_history:
+        if i < 10:
+            ax.plot(stock, label=f'$x_{{ {i + 1} }}$, {name_data[i]}')
+        elif 10 <= i < 20:
+            ax.plot(stock, label=f'$x_{{ {i + 1} }}$, {name_data[i]}', linestyle='--')
+        else:
+            ax.plot(stock, label=f'$x_{{ {i + 1} }}$, {name_data[i]}', linestyle=':')
+        i = i + 1
+    ax.set_ylabel('Del av portfölj, andel av 1')
+    ax.set_xlabel('Iteration $k$')
+    ax.set_title('Portföljkomposition $x^{(k)}$, agnostic, $\\alpha_2$')
+    fig.legend(fontsize = 'xx-small', loc='outside right upper', labelspacing=0.2,)
+    fig.savefig(f"composition/{ITERATIONS}-iter-composition-agnostic-alpha2.png", dpi=400)
 
 main()
